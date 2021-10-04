7087547658b934e592905b80961a98a82469e282
Alvaro <alvaro.rivas.p@gmail.com>
2021-10-04 01:00:50 -0300

    Desafio completo

---
+++ src/components/Task.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <li>{{ tarea }} <button @click="clickedBorrar(indice)">Borrar</button></li>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    tarea: String,
+    indice: Number,
+  },
+  methods: {
+    clickedBorrar(indice) {
+      this.$emit("borrar", indice);
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    li {
+    display: inline-block;
+    margin: 0.5rem;
+    }
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div>
+      <label for="tarea">Tarea:</label>
+      <input id="tarea" type="text" v-model="inputMensaje" placeholder="Agregar nueva tarea"/>
+      <button v-on:click="agregar">Agregar</button>
+    </div>
+    <hr>
+    <h3>Lista</h3>
+    <ul>
+      <Task 
+        v-for="(tarea, index) in lista" 
+        :key="index"
+        :tarea="tarea"
+        :indice="index"  
+        v-on:borrar="borrar"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+export default {
+  name: 'TaskList',
+  components: {
+    Task    
+  },
+  data(){
+    return {
+      lista: [],
+      inputMensaje: ''
+    }
+  },
+  methods: {
+    agregar(){
+      if(this.inputMensaje){
+        this.lista.push(this.inputMensaje);
+        this.inputMensaje = "";
+      }
+    },
+    borrar(id) {
+      this.lista = this.lista.filter((k, v) => v !== id);
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
